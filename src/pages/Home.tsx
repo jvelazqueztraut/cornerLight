2481e7dc78607c3ff2cde27d78df81bc15749336
Javier Velazquez Traut <jvt@fb.com>
2020-05-25 12:07:30 +0100

    Wizard; list app.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,57 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import MessageListItem from '../components/MessageListItem';
+import React, { useState } from 'react';
+import { Message, getMessages } from '../data/messages';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+  useIonViewWillEnter
+} from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useIonViewWillEnter(() => {
+    const msgs = getMessages();
+    setMessages(msgs);
+  });
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete();
+    }, 3000);
+  };
+
   return (
-    <IonPage>
+    <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Inbox</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">
+              Inbox
+            </IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
+        <IonList>
+          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
+        </IonList>
       </IonContent>
     </IonPage>
   );
